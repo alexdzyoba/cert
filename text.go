2db60e3ee700fa6bdadb114b3fa4a3b19cd5abab
Alex Dzyoba <alex@dzyoba.com>
2025-08-07 23:16:09 +0200

    Add list and name formatting

--- text.go
+++ text.go
@@ -1,11 +1,14 @@
 package main
 
 import (
+	"crypto/x509/pkix"
 	"fmt"
+	"math"
 	"strings"
 	"time"
 
 	"github.com/charmbracelet/lipgloss"
+	"github.com/charmbracelet/lipgloss/list"
 	"github.com/charmbracelet/lipgloss/table"
 )
 
@@ -21,6 +24,8 @@ const (
 	VerboseLevel
 	FullLevel
 )
+
+const DefaultListLimit = 5
 
 var (
 	cell = lipgloss.NewStyle().Padding(0, 1)
@@ -68,26 +73,26 @@ func (p TextBundlePrinter) printCert(cert *Cert) string {
 			return cell
 		})
 
-	t.Row("Subject", ": "+formatSubject(cert))
-	t.Row("Issuer", ": "+cert.Issuer.CommonName)
-	t.Row("Valid", ": "+formatValidity(cert))
-	t.Row("Features", ": "+buildFeatures(cert))
+	t.Row("Subject", ": "+p.formatSubject(cert))
+	t.Row("Issuer", ": "+p.formatName(cert.Issuer))
+	t.Row("Valid", ": "+p.formatValidity(cert))
+	t.Row("Features", ": "+p.buildFeatures(cert))
+	b.WriteString(t.Render())
 	if len(cert.DNSNames) > 0 {
-		t.Row("DNS SANs:", ": "+strings.Join(cert.DNSNames, "\n"))
+		b.WriteString(cell.Render("\nDNS SANs:\n" + p.formatList(cert.DNSNames)))
 	}
 
-	b.WriteString(t.Render())
 	return b.String()
 }
 
-func formatValidity(cert *Cert) string {
+func (p TextBundlePrinter) formatValidity(cert *Cert) string {
 	if cert.expiresIn < 0 {
 		return fmt.Sprintf("%v, expired on %v", cert.validity, cert.NotAfter.Format("2006-02-01"))
 	}
 	return fmt.Sprintf("%v, expires in %v (%v)", cert.validity, cert.expiresIn, cert.NotAfter.Format("2006-02-01"))
 }
 
-func buildFeatures(cert *Cert) string {
+func (p TextBundlePrinter) buildFeatures(cert *Cert) string {
 	features := []string{}
 	if cert.isRoot {
 		features = append(features, "ROOT")
@@ -111,8 +116,12 @@ func buildFeatures(cert *Cert) string {
 	return strings.Join(features, ", ")
 }
 
-func formatSubject(cert *Cert) string {
-	subject := cert.Subject.CommonName
+func (p TextBundlePrinter) formatSubject(cert *Cert) string {
+	if p.level > BaseLevel {
+		return cert.Subject.String()
+	}
+
+	subject := p.formatName(cert.Subject)
 
 	sans := []string{}
 	if len(cert.DNSNames) > 0 {
@@ -133,4 +142,43 @@ func formatSubject(cert *Cert) string {
 	}
 
 	return fmt.Sprintf("%s (%v)", subject, strings.Join(sans, " "))
+}
+
+func (p TextBundlePrinter) formatName(name pkix.Name) string {
+	if p.level > BaseLevel {
+		return name.String()
+	}
+
+	s := []string{name.CommonName}
+	if len(name.OrganizationalUnit) > 0 {
+		s = append(s, name.OrganizationalUnit...)
+	}
+	if len(name.Organization) > 0 {
+		s = append(s, name.Organization...)
+	}
+	if len(name.Country) > 0 {
+		s = append(s, name.Country...)
+	}
+
+	return strings.Join(s, ", ")
+}
+
+func (p TextBundlePrinter) formatList(ss []string) string {
+	items := ss
+	limit := int(math.MaxInt32)
+	if p.level < VerboseLevel {
+		limit = DefaultListLimit
+	}
+
+	if len(ss) > limit {
+		items = ss[:limit]
+	}
+	more := len(ss) - limit
+
+	l := list.New(items).Enumerator(list.Dash)
+	if more > 0 {
+		l.Item(fmt.Sprintf("(... %d more)", more))
+	}
+
+	return l.String()
 }
